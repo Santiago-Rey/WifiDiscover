31d31d47c55b93096d08d647499530d0c3e4e1eb
Santiago Rey <dsrey9435@gmail.com>
2024-06-12 01:05:03 -0500

    - Se agrega identificador para cada dispositivo por medio de UUID - Se crea el metodo addDeviceMultiHop para retransmitir loss paquetes recibidos - Se crea metodo multihop enviar el ultimo mensaje paquete recibido - se modifica metodo discoverrlistener para validar la información de los paquetes recibidos

--- app/src/main/java/com/example/wifidiscover/MainActivity.kt
+++ app/src/main/java/com/example/wifidiscover/MainActivity.kt
@@ -10,7 +10,6 @@ import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceInfo
 import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceRequest
 import android.os.Build
 import android.os.Bundle
-import android.os.Handler
 import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Toast
@@ -26,7 +25,7 @@ import java.util.concurrent.TimeUnit
 class MainActivity : AppCompatActivity() {
 
     private lateinit var sharedPreferences: SharedPreferences
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     //WiFi Direct variables
     private lateinit var wifiManager: WifiManager
@@ -38,9 +37,8 @@ class MainActivity : AppCompatActivity() {
 
 
     private val connectedDevices: MutableList<WifiP2pDevice> = mutableListOf()
-    private var info : WifiFrame = WifiFrame()
+    private var info: WifiFrame = WifiFrame()
     private var selectedDevice: WifiP2pDevice? = null
-
 
 
     var text: CharSequence = "Activa el wifi"
@@ -62,11 +60,11 @@ class MainActivity : AppCompatActivity() {
         startDiscover()
 
         exqListener()
-        binding.peerListView.setOnItemClickListener{parent,viiew,pos,id ->
+        binding.peerListView.setOnItemClickListener { parent, viiew, pos, id ->
             selectedDevice = deviceArray[pos].device
             startTimer()
 
-           // updateDescription(pos)
+            // updateDescription(pos)
 
         }
 
@@ -77,11 +75,11 @@ class MainActivity : AppCompatActivity() {
     private var task: Runnable? = null
     private var interval: Long = 10000
     private val devicesWithReceivedMessages: MutableSet<String> = mutableSetOf()
+
+    //Realiza la busqueda de servicios cada x tiempo
     fun startDiscover() {
         // Crea un nuevo Runnable que se ejecutará después de cada intervalo
         task = Runnable {
-            // Llama al método que deseas ejecutar
-
             discoverServices()
             clearReceivedDevicesAfterDelay()
 
@@ -89,6 +87,7 @@ class MainActivity : AppCompatActivity() {
         // Programa el primer ciclo del temporizador con un retraso inicial de 0 y un intervalo especificado
         executor.scheduleAtFixedRate(task!!, 0, interval, TimeUnit.MILLISECONDS)
     }
+
     private fun startTimer() {
         executor.scheduleAtFixedRate({
 
@@ -118,16 +117,13 @@ class MainActivity : AppCompatActivity() {
         }, 500, TimeUnit.MILLISECONDS)
     }
 
-
-
-
-
-    fun exqListener(){
+    // Guarda el mensaje y lo envia al momento de oprimir el botón
+    fun exqListener() {
 
         binding.sendButton.setOnClickListener {
             val message = binding.messageEditText.text.toString()
 
-            if(message.isNotBlank()){
+            if (message.isNotBlank()) {
                 Toast.makeText(
                     applicationContext,
                     "Mensaje guardado",
@@ -135,11 +131,11 @@ class MainActivity : AppCompatActivity() {
                 ).show()
                 saveMessage(message)
 
-                clearLocalServices{
+                clearLocalServices {
                     startRegistration()
                 }
 
-            }else{
+            } else {
                 Toast.makeText(
                     applicationContext,
                     "Guarda un mensaje",
@@ -148,74 +144,60 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-      /*  if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
-            wifiManager.isWifiEnabled = true
+        /*  if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+              wifiManager.isWifiEnabled = true
 
-        } else if (!wifiManager.isWifiEnabled) {
-            //Wifi activation
-            val toast: Toast = Toast.makeText(applicationContext, text, duration)
-            toast.show()
-        }*/
+          } else if (!wifiManager.isWifiEnabled) {
+              //Wifi activation
+              val toast: Toast = Toast.makeText(applicationContext, text, duration)
+              toast.show()
+          }*/
 
 
         binding.discover.setOnClickListener {
 
-            //init()
-            //addServiceRequest()
-            //startRegistration()
             clearServiceRequests {
                 addServiceRequest()
             }
 
-       }
+        }
 
-        binding.refresh.setOnClickListener{
+        binding.refresh.setOnClickListener {
             discoverServices()
         }
 
     }
 
-    fun init(){
+    //Configuración para utilizar los servicio de WifiP2p
+    fun init() {
 
         wifiManager = this.applicationContext.getSystemService(WIFI_SERVICE) as WifiManager
         mManager = getSystemService(WIFI_P2P_SERVICE) as WifiP2pManager
         mChannel = mManager.initialize(this, mainLooper, null)
 
+        WifiFrameUtils.getUUIDWiFiFrame(this)
 
-
-        val wInfo = wifiManager.connectionInfo
-        val macAddress = wInfo.macAddress
-
-       // expandableListAdapter = IngredientAdapter(this, collection)
-       // binding.peerListView.setAdapter(expandableListAdapter)
-        Log.e(TAG, "MAC Address : $macAddress")
-
-        /*binding.peerListView.setOnItemClickListener{_,_,pos,_ ->
-
-            updateDescription(pos)
-
-        }*/
 
     }
 
     private fun addDeviceList(record: WifiP2pDevice, wifiFrame: WifiFrame) {
 
-        val deviceSame = deviceArray.firstOrNull{it.device.deviceName == record.deviceName}
+        val deviceSame = deviceArray.firstOrNull { it.device.deviceName == record.deviceName }
 
-        if( deviceSame != null){
+        if (deviceSame != null) {
             val messageExist = deviceSame.message.any {
                 it.dateSend == wifiFrame.dateSend
             }
 
-            if(!messageExist)
-                 deviceSame.message.add(wifiFrame)
+            if (!messageExist)
+                deviceSame.message.add(wifiFrame)
 
-        }else{
-            var message = MessageModel(record, mutableListOf(wifiFrame) )
+        } else {
+            var message =
+                MessageModel(record, mutableListOf(wifiFrame), WifiFrameUtils.deviceIdMultiHop)
 
             deviceArray.add(message)
         }
-
 
         val adapter = ArrayAdapter(
             this,
@@ -224,46 +206,70 @@ class MainActivity : AppCompatActivity() {
 
         )
         binding.peerListView.adapter = adapter
-       // adapter.notifyDataSetChanged()
+        multihop(WifiFrameUtils.deviceIdMultiHop)
+    }
 
+    private fun addDeviceMultiHop(record: WifiP2pDevice, wifiFrame: WifiFrame) {
 
-            //discoverPeers()
+        val deviceSame = deviceArray.firstOrNull { it.device.deviceName == record.deviceName }
+        if (deviceSame != null) {
+            val messageExist = deviceSame.message.any {
+                it.dateSend == wifiFrame.dateSend
+            }
 
+            if (!messageExist) {
+                Toast.makeText(
+                    this,
+                    "Se ha agregado el mensaje retransmitido por " + record.deviceName,
+                    Toast.LENGTH_SHORT
+                ).show()
+                deviceSame.message.add(wifiFrame)
+            }
+
+        } else {
+            var device =
+                MessageModel(record, mutableListOf(wifiFrame), WifiFrameUtils.deviceIdMultiHop)
+
+            deviceArray.add(device)
+            Toast.makeText(
+                this,
+                "Se ha agregado el dispositivo retransmitido por " + record.deviceName,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            deviceArray
+
+        )
+        binding.peerListView.adapter = adapter
 
     }
 
 
-    private fun saveMessage(message: String){
+    private fun saveMessage(message: String) {
         val editor = sharedPreferences.edit()
         editor.putString(Constants.MESSAGE, message.trim())
         editor.apply()
     }
 
 
-    private fun updateDescription(selectedDevice: Int){
+    private fun updateDescription(selectedDevice: Int) {
 
         val selectedDeviceInfo = deviceArray[selectedDevice]
         binding.messageTextView.text = selectedDeviceInfo.getAllMessage()
-       /* val deviceInfo = "Nombre: ${wifiP2pDevice.deviceName}"
-        val deviceMAC = "MAC: ${wifiP2pDevice.deviceAddress}"
-        val message =  "Mensaje:"
-        //val messageSend = "Mensaje: "
-        val formattedDateTime = getFormattedDateTime() // Obtener la hora y la fecha actualizada*/
-
-        // Actualizar el TextView con la información del dispositivo y la hora/fecha actualizada
-        // binding.messageTextView.text = "$deviceNameArray"
-
 
     }
 
-
-
-    //Agregando servicio local
+    //Agregando servicio local para ofrecer la información del dispositivo actual a traves del servicio
     private fun startRegistration() {
 
-        info = WifiFrameUtils.buildMyWiFiFrame( this)
-        //Guardar el nombre del dispositvo y la MAC
-        val record =  WifiFrameUtils.wifiFrameToHashMap(info)
+
+        info = WifiFrameUtils.buildMyWiFiFrame(this)
+
+        val record = WifiFrameUtils.wifiFrameToHashMap(info)
 
 
         // Service information.  Pass it an instance name, service type
@@ -278,25 +284,69 @@ class MainActivity : AppCompatActivity() {
                 this,
                 Manifest.permission.NEARBY_WIFI_DEVICES
             ) != PackageManager.PERMISSION_GRANTED
-        )) {
+                    )
+        ) {
 
-            Toast.makeText(this, "Faltan pemisos 1" , Toast.LENGTH_SHORT ).show()
+            Toast.makeText(this, "Faltan pemisos 1", Toast.LENGTH_SHORT).show()
             return
         }
         mManager.addLocalService(mChannel, serviceInfo, object : WifiP2pManager.ActionListener {
             override fun onSuccess() {
-                // Command successful! Code isn't necessarily needed here,
-                // Unless you want to update the UI or add logging statements.
-                //discoverPeers()
+
             }
 
             override fun onFailure(arg0: Int) {
                 // Command failed.  Check for P2P_UNSUPPORTED, ERROR, or BUSY
-                Toast.makeText(this@MainActivity, "Fail local service" , Toast.LENGTH_SHORT ).show()
+                Toast.makeText(this@MainActivity, "Fail local service", Toast.LENGTH_SHORT).show()
             }
         })
     }
 
+    private fun multihop(deviceId: String) {
+
+        var deviceMulti = deviceArray.first {
+            it.id == deviceId
+        }
+
+        val record = WifiFrameUtils.wifiFrameToHashMapMultihop(
+            deviceMulti.device,
+            deviceMulti.message.last().sendMessage,
+            deviceMulti.id,
+            deviceMulti.message.last().dateSend
+        )
+
+
+        // Service information.  Pass it an instance name, service type
+        val serviceInfo =
+            WifiP2pDnsSdServiceInfo.newInstance("_networkChat", "_chatApp._tcp", record)
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED || (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
+                    && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.NEARBY_WIFI_DEVICES
+            ) != PackageManager.PERMISSION_GRANTED
+                    )
+        ) {
+
+            Toast.makeText(this, "Faltan pemisos 1", Toast.LENGTH_SHORT).show()
+            return
+        }
+        mManager.addLocalService(mChannel, serviceInfo, object : WifiP2pManager.ActionListener {
+            override fun onSuccess() {
+
+            }
+
+            override fun onFailure(arg0: Int) {
+                // Command failed.  Check for P2P_UNSUPPORTED, ERROR, or BUSY
+                Toast.makeText(this@MainActivity, "Fail local service", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    //Configura la solicitud para realizar el descubrimiento e informa si la solictud se realizo correctamente
     private fun addServiceRequest() {
         val serviceRequest = WifiP2pDnsSdServiceRequest.newInstance(
             "_networkChat",
@@ -313,7 +363,8 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 override fun onFailure(code: Int) {
-                    Toast.makeText(this@MainActivity, "Failure addService" , Toast.LENGTH_SHORT ).show()
+                    Toast.makeText(this@MainActivity, "Failure addService", Toast.LENGTH_SHORT)
+                        .show()
                     Log.e(TAG_WIFI, "Add service request has failed. $code")
                 }
             }
@@ -321,17 +372,25 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
-    //onreceivelocation en BP
+    //Gestiona la información de los servicios recibidos
     private fun discoverListener() {
 
         val txtListener = WifiP2pManager.DnsSdTxtRecordListener { fullDomain, record, srcDevice ->
             Log.d(TAG, "DnsSdTxtRecord available -$record")
-
-            if (record.isEmpty() || srcDevice.deviceName == "") return@DnsSdTxtRecordListener
-
             val wifiFrame = WifiFrameUtils.hashMapToWiFiFrame(record)
-            addDeviceList(srcDevice, wifiFrame)
+            if (record.isEmpty() || srcDevice.deviceName == "" || WifiFrameUtils.deviceIdMultiHop == WifiFrameUtils.idDevice.toString() || wifiFrame.sendMessage.isEmpty()) return@DnsSdTxtRecordListener
+
+            if (WifiFrameUtils.deviceMultihop.isNotEmpty()) {
+                val deviceP2p = WifiP2pDevice().apply {
+                    deviceName = WifiFrameUtils.deviceMultihop
+                }
+                wifiFrame.apply {
+                    nameMultiHop = srcDevice.deviceName
+                }
+                addDeviceMultiHop(deviceP2p, wifiFrame)
+            } else {
+                addDeviceList(srcDevice, wifiFrame)
+            }
 
 
             //Toast.makeText(this, "Servicio encontrado : mensaje ${wifiFrame.sendMessage}" , Toast.LENGTH_SHORT ).show()
@@ -359,10 +418,10 @@ class MainActivity : AppCompatActivity() {
             ) != PackageManager.PERMISSION_GRANTED)
         ) {
 
-            Toast.makeText(this, "Faltan pemisos 3" , Toast.LENGTH_SHORT ).show()
+            Toast.makeText(this, "Faltan pemisos 3", Toast.LENGTH_SHORT).show()
             return
         }
-            mManager.discoverPeers(
+        mManager.discoverPeers(
             mChannel,
             object : WifiP2pManager.ActionListener {
                 override fun onSuccess() {
@@ -376,21 +435,23 @@ class MainActivity : AppCompatActivity() {
                         ) != PackageManager.PERMISSION_GRANTED)
                     ) {
 
-                        Toast.makeText(this@MainActivity, "Faltan pemisos 7" , Toast.LENGTH_SHORT ).show()
+                        Toast.makeText(this@MainActivity, "Faltan pemisos 7", Toast.LENGTH_SHORT)
+                            .show()
                         return
                     }
 
-                //    mManager.requestPeers(mChannel, this@MainActivity)
+                    //    mManager.requestPeers(mChannel, this@MainActivity)
                 }
 
                 override fun onFailure(error: Int) {
-                    Toast.makeText(this@MainActivity, "failure peers" , Toast.LENGTH_SHORT ).show()
+                    Toast.makeText(this@MainActivity, "failure peers", Toast.LENGTH_SHORT).show()
                     Log.e(TAG_WIFI, "Discover peers has failed. $error")
                 }
             })
     }
 
 
+    //Inicia el descubrimiento
     private fun discoverServices() {
         if (ActivityCompat.checkSelfPermission(
                 this,
@@ -402,18 +463,18 @@ class MainActivity : AppCompatActivity() {
             ) != PackageManager.PERMISSION_GRANTED)
         ) {
 
-            Toast.makeText(this, "Faltan pemisos 4" , Toast.LENGTH_SHORT ).show()
+            Toast.makeText(this, "Faltan pemisos 4", Toast.LENGTH_SHORT).show()
             return
         }
         mManager.discoverServices(
             mChannel,
             object : WifiP2pManager.ActionListener {
                 override fun onSuccess() {
-                    Toast.makeText(this@MainActivity, "success discover" , Toast.LENGTH_SHORT ).show()
+                    Toast.makeText(this@MainActivity, "success discover", Toast.LENGTH_SHORT).show()
                 }
 
                 override fun onFailure(code: Int) {
-                    Toast.makeText(this@MainActivity, "Failure discover" , Toast.LENGTH_SHORT ).show()
+                    Toast.makeText(this@MainActivity, "Failure discover", Toast.LENGTH_SHORT).show()
                     Log.e(TAG_WIFI, "Discover services has failed. $code")
                 }
             }
@@ -425,13 +486,18 @@ class MainActivity : AppCompatActivity() {
             object : WifiP2pManager.ActionListener {
                 override fun onSuccess() {
                     Log.d("success", "clearServiceRequests result: Success")
-                    Toast.makeText(this@MainActivity, "Success clear request", Toast.LENGTH_SHORT ).show()
+                    Toast.makeText(this@MainActivity, "Success clear request", Toast.LENGTH_SHORT)
+                        .show()
                     onSuccessCallback.invoke()
                 }
 
                 override fun onFailure(code: Int) {
                     Log.e("failed", "clearServiceRequests result: Failure with code $code")
-                    Toast.makeText(this@MainActivity, "Failed to clear service requests: $code", Toast.LENGTH_SHORT ).show()
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Failed to clear service requests: $code",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             })
     }
@@ -441,13 +507,21 @@ class MainActivity : AppCompatActivity() {
             object : WifiP2pManager.ActionListener {
                 override fun onSuccess() {
                     Log.d("success", "clearLocalServices result: Success")
-                    Toast.makeText(this@MainActivity, "Success clear local services", Toast.LENGTH_SHORT ).show()
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Success clear local services",
+                        Toast.LENGTH_SHORT
+                    ).show()
                     onSuccessCallback.invoke()
                 }
 
                 override fun onFailure(code: Int) {
                     Log.e("Failed", "clearLocalServices result:  Failure with code $code")
-                    Toast.makeText(this@MainActivity, "Failed to clear local services: $code", Toast.LENGTH_SHORT ).show()
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Failed to clear local services: $code",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             })
     }
